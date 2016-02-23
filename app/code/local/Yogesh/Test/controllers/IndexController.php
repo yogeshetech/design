ae98528045cc3925673ea87a699631502ce33911
yogeshetech <yogesh@excellencetechnologies.in>
2016-02-23 18:38:10 +0530

    new file

---
+++ app/code/local/Yogesh/Test/controllers/IndexController.php
@@ -0,0 +1,85 @@
+<?php
+
+class Yogesh_Test_IndexController extends Mage_Core_Controller_Front_Action {
+
+    public function indexAction() {
+        
+      
+      //Insert Data  
+        if(isset($_POST['submit'])){
+            
+               $data = array(
+            "title" => $_POST['title'],
+            "filename" => $_POST['filename'],
+            "content" => $_POST['contents'],
+            "status" => $_POST['status'],
+            "created_time " => $_POST['time1'],
+            "update_time " => $_POST['time2']
+        );
+            
+            
+        $model = Mage::getModel('test/test')->insertData($data);
+        try {
+           // $id = $model->save()->getId();
+            echo "Data inserted successfully";
+            $url = 'http://127.0.0.1/etechmagento/index.php/test';
+           Mage::app()->getFrontController()->getResponse()->setRedirect($url);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            $url = 'http://127.0.0.1/etechmagento/index.php/test';
+           Mage::app()->getFrontController()->getResponse()->setRedirect($url);
+        }
+        }
+        
+        
+         
+        // Delete Data
+        
+        $id = $this->getRequest()->getParam('id');
+        if (!empty($id)) {
+            $model = Mage::getModel('test/test')->deleteData($id);
+            try {
+               // $model->setId($id)->delete();
+                echo "Data deleted successfully.";
+                $url = 'http://127.0.0.1/etechmagento/index.php/test';
+                Mage::app()->getFrontController()->getResponse()->setRedirect($url);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                $url = 'http://127.0.0.1/etechmagento/index.php/test';
+                Mage::app()->getFrontController()->getResponse()->setRedirect($url);
+            }
+        }
+       
+        
+              
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        $this->loadLayout();
+        $this->renderLayout();
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+        
+
+}
