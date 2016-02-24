ddac5384308d54359bfdef9da20c1c2745b52571
yogeshetech <yogesh@excellencetechnologies.in>
2016-02-24 19:18:34 +0530

    as

--- app/code/local/Excellence/Newtest/controllers/IndexController.php
+++ app/code/local/Excellence/Newtest/controllers/IndexController.php
@@ -49,7 +49,7 @@ class Excellence_Newtest_IndexController extends Mage_Core_Controller_Front_Acti
             $model = Mage::getModel('newtest/newtest')->deleteData($id);
             try {
                
-                Mage::getSingleton('newtest/newtest')->addSuccess(Mage::helper('contacts')->__('Selected row deleted'));
+                Mage::getSingleton('newtest/newtest')->addSuccess(Mage::helper('newtest')->__('Selected row deleted'));
                 $this->_redirect('newtest');
             } catch (Exception $e) {
                 echo $e->getMessage();
@@ -69,7 +69,7 @@ class Excellence_Newtest_IndexController extends Mage_Core_Controller_Front_Acti
 
             try {
                
-               Mage::getSingleton('newtest/newtest')->addSuccess(Mage::helper('contacts')->__('Data Inserted Succesfully.'));
+               Mage::getSingleton('newtest/newtest')->addSuccess(Mage::helper('newtest')->__('Data Inserted Succesfully.'));
                 $this->_redirect('newtest');
             } catch (Exception $e) {
                 echo $e->getMessage();
@@ -87,7 +87,7 @@ class Excellence_Newtest_IndexController extends Mage_Core_Controller_Front_Acti
         if (!empty($post['title'])) {
             $model = Mage::getModel('newtest/newtest')->myupdateData($post, $edit_id);
             try {
-                 Mage::getSingleton('newtest/newtest')->addSuccess(Mage::helper('contacts')->__('Data Updated.'));
+                 Mage::getSingleton('newtest/newtest')->addSuccess(Mage::helper('newtest')->__('Data Updated.'));
                 $this->_redirect('newtest');
             } catch (Exception $e) {
                 echo $e->getMessage();
